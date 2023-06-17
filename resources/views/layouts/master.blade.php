899cdd40e34a588f5689c8b15e7c46533f11468a
ZeroToZero <98218347+ImranMuhamadR@users.noreply.github.com>
2023-06-17 15:26:20 +0700

    cut sidebar menu to file sidebar.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -43,6 +43,7 @@
 
   {{-- Load dari layouts header.blade.php --}}
   @includeIf('layouts.header', ['some' => 'data'])
+  
   <!-- Left side column. contains the logo and sidebar -->
   <aside class="main-sidebar">
     <!-- sidebar: style can be found in sidebar.less -->
