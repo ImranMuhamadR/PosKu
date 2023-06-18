9d76950e623e0e1fa4f54b2a9de335e45c93baf9
ZeroToZero <98218347+ImranMuhamadR@users.noreply.github.com>
2023-06-18 21:11:03 +0700

    add file footer.blade.php

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,10 @@
+            <!-- /.content-wrapper -->
+            <footer class="main-footer">
+                <div class="pull-right hidden-xs">
+                    <b>Version</b>
+                    2.4.18
+                </div>
+                <strong>Copyright &copy; 2023
+                    <a href="https://adminlte.io">AdminLTE</a>.</strong>
+                All rights reserved.
+            </footer>
