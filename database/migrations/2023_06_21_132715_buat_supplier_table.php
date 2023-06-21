3ebce74a41bd646add4b32d81280bce2e5e608e8
ZeroToZero <98218347+ImranMuhamadR@users.noreply.github.com>
2023-06-21 20:41:49 +0700

    add file supplier

---
+++ database/migrations/2023_06_21_132715_buat_supplier_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class BuatSupplierTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //ketika migrate akan menambahkan tabel supplier
+        Schema::create('supplier', function (Blueprint $table) {
+            $table->increments('id_supplier');
+            $table->string('nama');
+            $table->text('alamat')->nullable();
+            $table->string('telepon');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //ketika migrate:rollback akan menghapus table supplier
+        Schema::dropIfExists('supplier');
+    }
+}
