fded2d0739f40ca377d4790a656cd1bcf8cc4ca4
ZeroToZero <98218347+ImranMuhamadR@users.noreply.github.com>
2023-06-21 22:07:35 +0700

    add file penjualan table

---
+++ database/migrations/2023_06_21_135414_buat_penjualan_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class BuatPenjualanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //ketika migrate akan menambahkan kolom dan table penjualan
+        Schema::create('penjualan', function (Blueprint $table) {
+            $table->increments('id_penjualan');
+            $table->integer('id_member');
+            $table->integer('total_item');
+            $table->integer('total_harga');
+            $table->tinyInteger('diskon')->default(0);
+            $table->integer('bayar')->default(0);
+            $table->integer('diterima')->default(0);
+            $table->integer('id_user');
+            
+            $table->timestamps();
+        }); 
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //ketika migrate:rollback akan menghapus table penjualan
+        Schema::dropIfExists('pembelian');
+
+    }
+}
