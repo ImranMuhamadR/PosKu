2ecc6e1f22c26f823f828bf50a74a7d1e0a0f536
ZeroToZero <98218347+ImranMuhamadR@users.noreply.github.com>
2023-06-21 22:08:14 +0700

    add file pembelian table

---
+++ database/migrations/2023_06_21_133849_buat_pembelian_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class BuatPembelianTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //ketika migrate akan menambahkan kolom dan table pembelian 
+        Schema::create('pembelian', function (Blueprint $table) {
+            $table->increments('id_pembelian');
+            $table->integer('id_supplier');
+            $table->integer('total_item');
+            $table->integer('total_harga');
+            $table->tinyInteger('diskon')->default(0);
+            $table->integer('bayar')->default(0);
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //ketika migrate:rollback akan menghapus table pembelian
+        Schema::dropIfExists('pembelian');
+    }
+}
