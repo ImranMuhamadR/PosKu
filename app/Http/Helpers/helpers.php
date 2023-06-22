8856f19ed0eae2f31b263b0b5bdfd9ebb7b7264b
ZeroToZero <98218347+ImranMuhamadR@users.noreply.github.com>
2023-06-22 20:36:46 +0700

    modifikasi helpers dan menambahkan fungsi format tanggal indo

--- app/Http/Helpers/helpers.php
+++ app/Http/Helpers/helpers.php
@@ -1,14 +1,10 @@
 <?php
 
 // fungsi ini akan menformat bilangan desimal dan akan mengembaliakan nilai sesuai format yang di tentukannya
-/*
-!contoh 
-misalkan kita berikan argument ke paramerter angka (100000).
-maka secara otomatis parameter angka akan di format menjadi (100.000)
-*/ 
 function format_uang($angka){
     return number_format($angka, 0, ',', '.');
 }
+// fungsi ini akan menghitung bilangan dalam bentuk string
 function terbilang($angka){
     $angka = abs($angka);
     $baca = array('', 'Satu', 'Dua', 'Tiga', 'Empat', 'Lima', 'Enam', 'Tujuh', 'Delapan', 'Sembilan', 'Sepuluh', 'Sebelas' );
@@ -31,4 +27,23 @@ function terbilang($angka){
     $terbilang = terbilang($angka / 1000000) . ' Juta' . terbilang($angka % 1000000);
     
     return $terbilang;
+}
+// fungsi ini akan membuat tanggal indonesia
+function tanggal_indo($tgl, $show_hari = true){
+    $nama_hari = array('Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu');
+    $nama_bulan = array(1>= 'Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember');
+    $tahun = substr($tgl, 0, 4);//2021-03-07
+    $bulan = $nama_bulan[(int) substr($tgl, 5, 2)];
+    $tanggal = substr($tgl, 8, 2);
+    $text = '';
+
+    if($show_hari){
+        $urutan_hari = date('w', mktime(0,0,0, substr($tgl, 5, 2), $tanggal, $tahun));
+        $hari = $nama_hari[$urutan_hari];
+        $text .= "$hari, $tanggal $bulan $tahun";
+
+    }else{
+        $text .= "$tanggal $bulan $tahun";
+    }
+    return $text;
 }
