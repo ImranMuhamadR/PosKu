1fbb15bd20d978a1c808c803ab741d6597ffe882
ZeroToZero <98218347+ImranMuhamadR@users.noreply.github.com>
2023-07-06 16:22:13 +0700

    change file helpers.php

--- app/Http/Helpers/helpers.php
+++ app/Http/Helpers/helpers.php
@@ -29,23 +29,29 @@ function terbilang($angka){
     return $terbilang;
 }
 // fungsi ini akan membuat tanggal indonesia
-function tanggal_indo($tgl, $show_hari = true){
-    $nama_hari = array('Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu');
-    $nama_bulan = array(1>= 'Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember');
-    $tahun = substr($tgl, 0, 4);//2021-03-07
-    $bulan = $nama_bulan[(int) substr($tgl, 5, 2)];
+function tanggal_indo($tgl, $tampil_hari = true)
+{
+    $nama_hari  = array(
+        'Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jum\'at', 'Sabtu'
+    );
+    $nama_bulan = array(1 =>
+        'Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'
+    );
+
+    $tahun   = substr($tgl, 0, 4);
+    $bulan   = $nama_bulan[(int) substr($tgl, 5, 2)];
     $tanggal = substr($tgl, 8, 2);
-    $text = '';
+    $text    = '';
 
-    if($show_hari){
+    if ($tampil_hari) {
         $urutan_hari = date('w', mktime(0,0,0, substr($tgl, 5, 2), $tanggal, $tahun));
-        $hari = $nama_hari[$urutan_hari];
-        $text .= "$hari, $tanggal $bulan $tahun";
-
-    }else{
-        $text .= "$tanggal $bulan $tahun";
+        $hari        = $nama_hari[$urutan_hari];
+        $text       .= "$hari, $tanggal $bulan $tahun";
+    } else {
+        $text       .= "$tanggal $bulan $tahun";
     }
-    return $text;
+    
+    return $text; 
 }
 
 function tambah_nol_didepan($value, $threshold = null){
