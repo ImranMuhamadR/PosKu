031b95163c64b6939389d12c098086dd7245fd2b
Imran Muhamad Rafi <imranmuhamadrafi7@gmail.com>
2023-07-16 02:16:25 +0700

    modifikasi views layout master.blade

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -20,10 +20,6 @@
         <link
             rel="stylesheet"
             href="{{asset('AdminLTE/bower_components/font-awesome/css/font-awesome.min.css')}}">
-        <!-- Ionicons -->
-        <link
-            rel="stylesheet"
-            href="{{asset('AdminLTE/bower_components/Ionicons/css/ionicons.min.css')}}">
         <!-- Theme style -->
         <link rel="stylesheet" href="{{asset('AdminLTE/dist/css/AdminLTE.min.css')}}">
         <!-- AdminLTE Skins. Choose a skin from the css/skins folder instead of
@@ -31,27 +27,6 @@
         <link
             rel="stylesheet"
             href="{{asset('AdminLTE/dist/css/skins/_all-skins.min.css')}}">
-        <!-- Morris chart -->
-        <link
-            rel="stylesheet"
-            href="{{asset('AdminLTE/bower_components/morris.js/morris.css')}}">
-        <!-- jvectormap -->
-        <link
-            rel="stylesheet"
-            href="{{asset('AdminLTE/bower_components/jvectormap/jquery-jvectormap.css')}}">
-        <!-- Date Picker -->
-        <link
-            rel="stylesheet"
-            href="{{asset('AdminLTE/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css')}}">
-        <!-- Daterange picker -->
-        <link
-            rel="stylesheet"
-            href="{{asset('AdminLTE/bower_components/bootstrap-daterangepicker/daterangepicker.css')}}">
-        <!-- bootstrap wysihtml5 - text editor -->
-        <link
-            rel="stylesheet"
-            href="{{asset('AdminLTE/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css')}}">
-
         <!-- DataTables -->
         <link
             rel="stylesheet"
@@ -63,10 +38,13 @@
         <!--[if lt IE 9]> <script
         src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script> <script
         src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script> <![endif]-->
+
         <!-- Google Font -->
         <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+
+            @stack('css')
     </head>
     <body class="hold-transition skin-red-light sidebar-mini">
         <div class="wrapper">
@@ -104,69 +82,26 @@
                 <!-- /.content -->
             </div>
 
-            @includeIf('layouts.footer', ['some' => 'data'])
+            @includeIf('layouts.footer')
+            {{-- fix --}}
+            {{-- @includeIf('layouts.footer', ['some' => 'data']) --}}
 
         </div>
         <!-- ./wrapper -->
 
         <!-- jQuery 3 -->
-        <script src="{{asset('AdminLTE/bower_components/jquery/dist/jquery.min.js')}}"></script>
-        <!-- jQuery UI 1.11.4 -->
-        <script src="{{asset('AdminLTE/bower_components/jquery-ui/jquery-ui.min.js')}}"></script>
-        <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-        <script>
-            $
-                .widget
-                .bridge('uibutton', $.ui.button);
-        </script>
-        <!-- Bootstrap 3.3.7 -->
-        <script
-            src="{{asset('AdminLTE/bower_components/bootstrap/dist/js/bootstrap.min.js')}}"></script>
-        <!-- Morris.js charts -->
-        <script src="{{asset('AdminLTE/bower_components/raphael/raphael.min.js')}}"></script>
-        <script src="{{asset('AdminLTE/bower_components/morris.js/morris.min.js')}}"></script>
-        <!-- Sparkline -->
-        <script
-            src="{{asset('AdminLTE/bower_components/jquery-sparkline/dist/jquery.sparkline.min.js')}}"></script>
-        <!-- jvectormap -->
-        <script
-            src="{{asset('AdminLTE/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js')}}"></script>
-        <script
-            src="{{asset('AdminLTE/plugins/jvectormap/jquery-jvectormap-world-mill-en.js')}}"></script>
-        <!-- jQuery Knob Chart -->
-        <script
-            src="{{asset('AdminLTE/bower_components/jquery-knob/dist/jquery.knob.min.js')}}"></script>
-        <!-- daterangepicker -->
-        <script src="{{asset('AdminLTE/bower_components/moment/min/moment.min.js')}}"></script>
-        <script
-            src="{{asset('AdminLTE/bower_components/bootstrap-daterangepicker/daterangepicker.js')}}"></script>
-        <!-- datepicker -->
-        <script
-            src="{{asset('AdminLTE/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js')}}"></script>
-        <!-- Bootstrap WYSIHTML5 -->
-        <script
-            src="{{asset('AdminLTE/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js')}}"></script>
-        <!-- Slimscroll -->
-        <script
-            src="{{asset('AdminLTE/bower_components/jquery-slimscroll/jquery.slimscroll.min.js')}}"></script>
-        <!-- FastClick -->
-        <script src="{{asset('AdminLTE/bower_components/fastclick/lib/fastclick.js')}}"></script>
-        <!-- ChartJS -->
-        <script src="{{asset('AdminLTE/bower_components/chart.js/Chart.js')}}"></script>
-        <!-- DataTables -->
-        <script
-            src="{{asset('AdminLTE/bower_components/datatables.net/js/jquery.dataTables.min.js') }}"></script>
-        <script
-            src="{{asset('AdminLTE/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js') }}"></script>
-        <!-- AdminLTE App -->
-        <script src="{{asset('AdminLTE/dist/js/adminlte.min.js')}}"></script>
-        <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-        <script src="{{asset('AdminLTE/dist/js/pages/dashboard.js')}}"></script>
-        <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-        <script src="{{asset('AdminLTE/dist/js/pages/dashboard2.js')}}"></script>
-        {{-- Validator --}}
-        <script src="{{ asset('js/validator.min.js') }}"></script>
-
+    <script src="{{ asset('AdminLTE/bower_components/jquery/dist/jquery.min.js') }}"></script>
+    <!-- Bootstrap 3.3.7 -->
+    <script src="{{ asset('AdminLTE/bower_components/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+    <!-- Moment -->
+    <script src="{{ asset('AdminLTE/bower_components/moment/min/moment.min.js') }}"></script>
+    <!-- DataTables -->
+    <script src="{{ asset('AdminLTE/bower_components/datatables.net/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('AdminLTE/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js') }}"></script>
+    <!-- AdminLTE App -->
+    <script src="{{ asset('AdminLTE/dist/js/adminlte.min.js') }}"></script>
+    <!-- Validator -->
+    <script src="{{ asset('js/validator.min.js') }}"></script>
         @stack('scripts')
     </body>
 </html>
