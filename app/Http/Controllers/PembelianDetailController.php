ca2c6e44a8a54475d5d33a74c4f27af1439ef9b6
Imran Muhamad Rafi <imranmuhamadrafi7@gmail.com>
2023-07-16 16:24:36 +0700

    change file PembelianDetailController

--- app/Http/Controllers/PembelianDetailController.php
+++ app/Http/Controllers/PembelianDetailController.php
@@ -16,12 +16,13 @@ class PembelianDetailController extends Controller
         $id_pembelian = session('id_pembelian');
         $produk = Produk::orderBy('nama_produk')->get();
         $supplier = Supplier::find(session('id_supplier'));
-        // $diskon = Pembelian::find($id_pembelian)->diskon ?? 0;
+        // ini untuk secara otomatis akan update fild diskon 5% pada transaksi pembelian
+        $diskon = Pembelian::find($id_pembelian)->diskon ?? 0;
 
         if (! $supplier) {
             abort(404);
         }
-        return view('pembelian_detail.index', compact('id_pembelian', 'produk', 'supplier'));
+        return view('pembelian_detail.index', compact('id_pembelian', 'produk', 'supplier', 'diskon'));
     }
 
     public function data($id)
