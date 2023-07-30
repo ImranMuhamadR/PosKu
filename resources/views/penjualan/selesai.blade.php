f9bb7b278dad061e86b17775b5c1ea67299f9d87
Imran Muhamad Rafi <imranmuhamadrafi7@gmail.com>
2023-07-30 13:03:59 +0700

    modified view penjualan/selesai dan routes/web

--- resources/views/penjualan/selesai.blade.php
+++ resources/views/penjualan/selesai.blade.php
@@ -9,26 +9,70 @@
     <li class="active">Transaksi Penjualan</li>
 @endsection
 
+@push('css')
+<style>
+/* CSS */
+.container-center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 15vh; /* Menyesuaikan tinggi container sesuai dengan tinggi viewport */
+}
+
+.alert {
+  text-align: center; /* Mengatur teks menjadi tengah */
+  max-width: 400px; /* Atur lebar maksimum agar pesan tidak terlalu lebar */
+}
+
+.box-footer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 15vh; /* Menyesuaikan tinggi konten dengan tinggi viewport */
+}
+
+</style>
+@endpush
+
 @section('content')
 <div class="row">
     <div class="col-lg-12">
         <div class="box">
             <div class="box-body">
-                <div class="alert alert-success alert-dismissible">
+                {{-- fix --}}
+                {{-- <div class="alert alert-success alert-dismissible">
                     <i class="fa fa-check icon"></i>
-                    Data Transaksi telah selesai.
+                    Yeyys Transaksi Berhasil Disimpan.
+                </div> --}}
+                <div class="container-center">
+                    <div class="alert alert-success alert-dismissible">
+                        <i class="fa fa-check icon"></i>
+                        Yeyys Transaksi Berhasil Disimpan.
+                    </div>
                 </div>
+                
             </div>
-            <div class="box-footer">
+            {{-- fix --}}
+            {{-- <div class="box-footer">
                 @if ($setting->tipe_nota == 1)
-                <button class="btn btn-warning btn-flat" onclick="notaKecil('{{ route('transaksi.nota_kecil') }}', 'Nota Kecil')">Cetak Ulang Nota</button>
+                <button class="btn btn-warning btn-flat" onclick="notaKecil('{{ route('transaksi.nota_kecil') }}', 'Nota Kecil')">Cetak Nota</button>
                 @else
-                <button class="btn btn-warning btn-flat" onclick="notaBesar('{{ route('transaksi.nota_besar') }}', 'Nota PDF')">Cetak Ulang Nota</button>
+                <button class="btn btn-warning btn-flat" onclick="notaBesar('{{ route('transaksi.nota_besar') }}', 'Nota PDF')">Cetak Nota</button>
                 @endif
                 <a href="{{ route('transaksi.baru') }}" class="btn btn-primary btn-flat">Transaksi Baru</a>
+            </div> --}}
+            <div class="box-footer">
+                    <div class="text-center">
+                        @if ($setting->tipe_nota == 1)
+                        <button class="btn btn-warning btn-flat" onclick="notaKecil('{{ route('transaksi.nota_kecil') }}', 'Nota Kecil')">Cetak Nota</button>
+                        @else
+                        <button class="btn btn-warning btn-flat" onclick="notaBesar('{{ route('transaksi.nota_besar') }}', 'Nota PDF')">Cetak Nota</button>
+                        @endif
+                        <a href="{{ route('transaksi.baru') }}" class="btn btn-primary btn-flat">Transaksi Baru</a>        
+                    </div>
             </div>
         </div>
-    </div>
+    </div> 
 </div>
 @endsection
 
@@ -45,6 +89,7 @@
         popupCenter(url, title, 900, 675);
     }
 
+    // menampilkan pop up center
     function popupCenter(url, title, w, h) {
         const dualScreenLeft = window.screenLeft !==  undefined ? window.screenLeft : window.screenX;
         const dualScreenTop  = window.screenTop  !==  undefined ? window.screenTop  : window.screenY;
--- routes/web.php
+++ routes/web.php
@@ -70,17 +70,22 @@ Route::group(['middleware' => 'auth'], function(){
     Route::get('/penjualan/data', [PenjualanController::class, 'data'])->name('penjualan.data');
     Route::get('/penjualan', [PenjualanController::class, 'index'])->name('penjualan.index');
     Route::get('/penjualan/{id}', [PenjualanController::class, 'show'])->name('penjualan.show');
-    // ! ini akan mendelete transaksi
-    Route::delete('/penjualan/{id}', [PenjualanController::class, 'destroy'])->name('penjualan.destroy');
+    Route::delete('/penjualan/{id}', [PenjualanController::class, 'destroy'])->name('penjualan.destroy');    // ! ini akan mendelete transaksi
+
 
 
     // ! Route ini akan mengakses method create yang ada pada PenjualanController
     Route::get('/transaksi/baru', [PenjualanController::class, 'create'])->name('transaksi.baru');
     Route::post('/transaksi/simpan', [PenjualanController::class, 'store'])->name('transaksi.simpan');
+    Route::get('/transaksi/selesai', [PenjualanController::class, 'selesai'])->name('transaksi.selesai');
+    Route::get('/transaksi/nota-kecil', [PenjualanController::class, 'notaKecil'])->name('transaksi.nota_kecil');
+    Route::get('/transaksi/nota-besar', [PenjualanController::class, 'notaBesar'])->name('transaksi.nota_besar');
+
+
     Route::get('/transaksi/{id}/data', [PenjualanDetailController::class, 'data'])->name('transaksi.data');
     Route::get('/transaksi/loadform/{id}/{total}/{diterima}', [PenjualanDetailController::class, 'loadForm'])->name('transaksi.load_form');
     Route::resource('/transaksi', PenjualanDetailController::class)
-    ->except('show');
+    ->except('create', 'show', 'edit');
 
     // ! Route ini akan mengakses method create yang ada pada MemberController
     Route::get('/member/data', [MemberController::class, 'data'])->name('member.data');
