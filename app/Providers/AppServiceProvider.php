9f3cd0948ed6cb6901fd7563e227b2a44ac9f9b9
Imran Muhamad Rafi <imranmuhamadrafi7@gmail.com>
2023-07-31 21:16:09 +0700

    modified AppSeviceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,12 +19,13 @@ class AppServiceProvider extends ServiceProvider
         view()->composer('layouts.master', function ($view) {
             $view->with('setting', Setting::first());
         });
-        // view()->composer('layouts.auth', function ($view) {
-        //     $view->with('setting', Setting::first());
-        // });
-        // view()->composer('auth.login', function ($view) {
-        //     $view->with('setting', Setting::first());
-        // });
+        // logo pada halaman login juga berubah
+        view()->composer('layouts.auth', function ($view) {
+            $view->with('setting', Setting::first());
+        });
+        view()->composer('auth.login', function ($view) {
+            $view->with('setting', Setting::first());
+        });
     }
 
     /**
