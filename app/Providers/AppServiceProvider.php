ad7152b962fdd11de24d964ad63e4f7cb16cd594
Imran Muhamad Rafi <imranmuhamadrafi7@gmail.com>
2023-07-31 10:49:02 +0700

    modified

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Models\Setting;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,6 +15,16 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+        // $setting = Setting::first();
+        view()->composer('layouts.master', function ($view) {
+            $view->with('setting', Setting::first());
+        });
+        // view()->composer('layouts.auth', function ($view) {
+        //     $view->with('setting', Setting::first());
+        // });
+        // view()->composer('auth.login', function ($view) {
+        //     $view->with('setting', Setting::first());
+        // });
     }
 
     /**
