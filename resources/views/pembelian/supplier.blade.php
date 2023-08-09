8a8b3344897ee299d040b9077b3843c531f4612b
Imran Muhamad Rafi <imranmuhamadrafi7@gmail.com>
2023-08-09 12:44:16 +0700

    change button pilih supplier in pembelian/supplier

--- resources/views/pembelian/supplier.blade.php
+++ resources/views/pembelian/supplier.blade.php
@@ -24,7 +24,7 @@
                                 <td>{{ $item->alamat }}</td>
                                 <td>
                                     {{-- ini akan mengarah ke transaksi pembelian --}}
-                                    <a href="{{ route('pembelian.create', $item->id_supplier) }}" class="btn btn-primary btn-xs btn-flat">
+                                    <a href="{{ route('pembelian.create', $item->id_supplier) }}" class="btn btn-warning btn-xs btn-flat">
                                         <i class="fa fa-check-circle"></i>
                                         Pilih
                                     </a>
